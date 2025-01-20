e2cf83aa0bc2219ab525e9ca9ce6ee0b21fec181
Natália Diotto <diottonatalia@gmail.com>
2025-01-20 19:58:53 -0300

    Game seed on import.sql

--- src/main/java/com/diotto/gamelist/entities/Game.java
+++ src/main/java/com/diotto/gamelist/entities/Game.java
@@ -12,10 +12,18 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+    private Double score;
     private Integer releaseYear;
+    private String genre;
     private String platform;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
+
+
 
 }
